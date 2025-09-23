30e86db2c699170c1cf72765ff5fa77577ad2758
Lance <chanlance.school@gmail.com>
2025-09-23 14:08:05 +0800

    feature(Weather_App): complete app

--- Weather_Project/Weather_Project/settings.py
+++ Weather_Project/Weather_Project/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
@@ -119,8 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
